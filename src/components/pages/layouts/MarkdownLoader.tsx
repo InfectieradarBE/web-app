58fe9e1d363c7ae6a433f092663be9a6e457a353
phev8 <phev8@users.noreply.github.com>
2021-01-28 22:43:18 +0100

    add hook to load "translated" markdowns

--- src/components/pages/layouts/MarkdownLoader.tsx
+++ src/components/pages/layouts/MarkdownLoader.tsx
@@ -1,47 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useTranslatedMarkdown } from '../../../hooks/useTranslatedMarkdown';
 import MarkdownRenderer from '../../displays/MarkdownRenderer';
 
 interface MarkdownLoaderProps {
-    className?: string;
-    markdownUrl: string;
-    languageCode: string;
+  className?: string;
+  markdownUrl: string;
+  languageCode: string;
 }
 
 const MarkdownLoader: React.FC<MarkdownLoaderProps> = (props) => {
-    const [content, setContent] = useState<string | undefined>();
+  const content = useTranslatedMarkdown(props.markdownUrl);
 
-    useEffect(() => {
-        const url = `${process.env.REACT_APP_CONTENT_URL}/locales/${props.languageCode}/${props.markdownUrl}`;
-        fetch(url)
-            .then(res => res.text())
-            .then(text => {
-                if (text.startsWith('<!DOCTYPE html>')) {
-                    console.log(`cannot find content for ${props.markdownUrl}`);
-                    return;
-                }
-                setContent(text)
-            })
-            .catch(error => console.log(error));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.languageCode, props.markdownUrl]);
-
-
-    if (!content) {
-        return null;
-    }
-    return <MarkdownRenderer
-        className={props.className}
-        markdown={content}
-        renderers={{
-            'inlineCode': (node) => <p className="border-primary border-top-2 border-bottom-2" style={{ fontSize: '1.0rem', color: '#696969' }} >{node.children}</p>,
-            'paragraph': (node) => node.children[0].type.name === "image" || node.children[0].type.name === "inlineCode" ? (
-                <div {...node} />
-            ) : (
-                    <p {...node} />
-                )
-            ,
-        }}
-    />
+  if (!content) {
+    return null;
+  }
+  return <MarkdownRenderer
+    className={props.className}
+    markdown={content}
+    renderers={{
+      'inlineCode': (node) => <p className="border-primary border-top-2 border-bottom-2" style={{ fontSize: '1.0rem', color: '#696969' }} >{node.children}</p>,
+      'paragraph': (node) => node.children[0].type.name === "image" || node.children[0].type.name === "inlineCode" ? (
+        <div {...node} />
+      ) : (
+          <p {...node} />
+        )
+      ,
+    }}
+  />
 };
 
 export default MarkdownLoader;
