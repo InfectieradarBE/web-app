561f94abffb030b03c981d25fb357368c0221830
phev8 <phev8@users.noreply.github.com>
2021-01-28 13:30:15 +0100

    Add and reference placeholder files for not implemented components

---
+++ src/components/displays/SystemInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface SystemInfoProps {
+}
+
+const SystemInfo: React.FC<SystemInfoProps> = (props) => {
+  return (
+    <p>SystemInfo</p>
+  );
+};
+
+export default SystemInfo;
---
+++ src/components/settings/AccountSettings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface AccountSettingsProps {
+}
+
+const AccountSettings: React.FC<AccountSettingsProps> = (props) => {
+  return (
+    <p>AccountSettings</p>
+  );
+};
+
+export default AccountSettings;
